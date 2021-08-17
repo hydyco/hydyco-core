0fbf68239c4c783959e204bcf77ba9584d5c9d80
iamrahultanwar <mr.rahultanwar@gmail.com>
2021-08-17 08:01:58 +0530

    👌 IMPROVE: removed build files

--- index.d.ts
+++
@@ -1,3 +0,0 @@
-import { HydycoFile, HydycoParser, HydycoPath } from "./utils";
-import { HydycoServer } from "./server";
-export { HydycoServer, HydycoFile, HydycoParser, HydycoPath };
--- utils/index.d.ts
+++
@@ -1,4 +0,0 @@
-import HydycoFile from "./file";
-import HydycoPath from "./path";
-import HydycoParser from "./parser";
-export { HydycoFile, HydycoParser, HydycoPath };
